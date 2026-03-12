0d7218ca6bbac3d663169f0044e135fdcc1ecd71
DimKing17 <edimnobi@gmail.com>
2026-03-12 10:21:08 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 // AUTO-GENERATED — do not edit manually. Run generate-config.js to regenerate.
 const FIREBASE_CONFIG = {
-  "apiKey": "",
-  "authDomain": "",
-  "projectId": "",
-  "storageBucket": "",
-  "messagingSenderId": "",
-  "appId": ""
+  "apiKey": "AIzaSyDD-036QmunaoGfSEem0hO2FLSrUaiKKlo",
+  "authDomain": "uniguide2-41a4b.firebaseapp.com",
+  "projectId": "uniguide2-41a4b",
+  "storageBucket": "uniguide2-41a4b.firebasestorage.app",
+  "messagingSenderId": "11867097005",
+  "appId": "1:11867097005:web:7ff1775dfab3a739568324"
 };
 const FIREBASE_CONFIGURED = false;
